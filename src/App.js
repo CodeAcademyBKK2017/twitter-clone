abdbf29a17a9b34066427bcb3f0efe0c768a8798
Rahul gaba <rahulgaba16@gmail.com>
2017-12-13 10:05:13 +0700

    Added Readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, {Component} from 'react';
+import {Icon} from 'react-fa';
+
 class App extends Component {
   render () {
     return (
       <div >
-        hello world
+        hello world <Icon name='rocket'></Icon>
       </div>
     );
   }
