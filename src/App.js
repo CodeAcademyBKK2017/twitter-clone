e4174ef9a9c5f4ebfb5300ec6afe1cfc35b7b838
Jack <jakkrit.chin@gmail.com>
2017-12-13 10:56:05 +0700

    complete layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React, {Component} from 'react';
 import {Icon} from 'react-fa';
 
 class App extends Component {
+
   render () {
     return (
-      <div >
-        hello world <Icon name='rocket'></Icon>
+      <div>
+        <div className="header" />
+        <div className="container">
+          <div className="menu" />
+          <div className="content" />
+        </div>
       </div>
     );
   }
+
 }
 
 export default App;
