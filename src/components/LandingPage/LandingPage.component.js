dfb3a321df7e4e0088fed4dfc170f521f042d748
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-12-13 12:25:13 +0700

    set layout header

--- src/components/LandingPage/LandingPage.component.js
+++ src/components/LandingPage/LandingPage.component.js
@@ -1,5 +1,6 @@
 import './LandingPage.style.css';
 import React, {Component} from 'react';
+import {Icon} from 'react-fa';
 // import PropTypes from 'prop-types';
 // import noop from 'lodash/noop';
 
@@ -9,12 +10,36 @@ class LandingPage extends Component {
 		
 		return (
 			<div className="container">
-				<div className="box twitter-header" />
+				<div className="box twitter-header">
+					<div className="left-content">
+						<button className="circle border twitter">
+							<Icon name="twitter"/>
+						</button>
+						<button className="circle border">
+							<Icon name="bell-o"/>
+						</button>
+						<button className="circle border">
+							<Icon name="envelope-o"/>
+						</button>
+						<button className="circle border">
+							<Icon name="hashtag"/>
+						</button>
+					</div>
+
+					<div className="logo-content">
+						<Icon name="twitter"/>
+					</div>
+
+					<div className="left-content">
+						<input className="search-box" type="text"/>
+						<button className=""/>
+					</div>
+				</div>
 				
 				<div className="main">
 					<div className="center">
-						<div className="box menu" />
-						<div className="box content" />
+						<div className="box menu"/>
+						<div className="box content"/>
 					</div>
 				</div>
 			</div>
