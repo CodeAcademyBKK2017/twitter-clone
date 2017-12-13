814b9102276aa580bfb4deb1b3a58519ca12e37c
chaowalit <dekchaowalit_123@hotmail.com>
2017-12-13 14:26:14 +0700

    update twitter to header

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,12 +1,15 @@
 import './Header.css';
 import React, {Component} from 'react';
+import {Icon} from 'react-fa';
 
 class Header extends Component {
   render () {
     return (
       <div className="header box-shadow background-white">
         <div className="left-head"/>
-        <div className="center-head"/>
+        <div className="center-head">
+          <Icon name="twitter" size="2x"/>
+        </div>
         <div className="right-head"/>
       </div>
     );
