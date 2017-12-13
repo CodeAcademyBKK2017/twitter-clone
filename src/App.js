3c217ec762791bdb97dd9690ad288eab0e2268ed
Rahul gaba <rahulgaba16@gmail.com>
2017-12-13 09:52:36 +0700

    Boilerplate added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, {Component} from 'react';
+class App extends Component {
+  render () {
+    return (
+      <div >
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
