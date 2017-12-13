02f6e78e8da27e9184d0c934e5595ec32bcbf15d
deathsky0725 <wbenjapol@gmail.com>
2017-12-13 04:44:33 +0000

    Update boxshadow

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import {Icon} from 'react-fa';
 class App extends Component {
   render () {
     return (
-      <div >
-        hello world <Icon name='rocket'></Icon>
+      <div className="app">
+        <div className="app-header" />
+        <div className="app-body">
+          <div className="app-header2" /> 
+          <div className="app-body2"/>
+        </div>
       </div>
     );
   }
 }
-
+/* <div > hello world <Icon name='rocket' /></div> */ 
 export default App;
