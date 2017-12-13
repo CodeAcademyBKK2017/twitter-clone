b68b6eea8661ceac1943186d90dbbaa5876085bb
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-12-13 14:26:15 +0700

    logo twitter

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Header.style.css';
+import {Icon} from 'react-fa';
 
 class Header extends Component {
   render () {
     return (
       <div className='header'>
         <div className='header--left'>left</div>
-        <div className='header--logo' />
+        <div className='header--logo'><Icon name="twitter" size="2x" color="#80A8CC" /></div>
         <div className='header--right'>right</div>
       </div>
     );
