0b431688073e5640776f42372b6f0da8f9cc7fac
Panurut <nubrabra5@gmail.com>
2017-12-13 11:48:04 +0700

    '13122017V1'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import React, {Component} from 'react';
-import {Icon} from 'react-fa';
+// import {Icon} from 'react-fa';
 
 class App extends Component {
   render () {
     return (
-      <div >
-        hello world <Icon name='rocket'></Icon>
+      <div className='App'>
+
+        <div className='App-header' />
+      
+        <div className='Box'>
+          <div className='Box-title'/>
+          <div className='Box-content'/>
+        </div>
+              
       </div>
     );
   }
