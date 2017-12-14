eda2cca3b2e575c02ddb5261f989e83770ddd281
Benjapol Suksakulchai <assassin@Benjapols-iMac.local>
2017-12-14 14:21:13 +0700

    all style clear

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import appStyle from './App.css';
 import React, {Component} from 'react';
 import Header from './components/Header/Header.component';
 class App extends Component {
   render () {
     return (
-      <div className="app">
+      <div className={appStyle.app}>
         <Header/>
-        <div className="app-body">
-          <div className="app-header2 shadow white-box" /> 
-          <div className="app-body2 shadow white-box"/>
+        <div className={appStyle.appBody}>
+          <div className={`${appStyle.header2} ${appStyle.shadow} ${appStyle.whiteBox}`} /> 
+          <div className={`${appStyle.appBody2} ${appStyle.shadow} ${appStyle.whiteBox}`}/>
         </div>
       </div>
     );
