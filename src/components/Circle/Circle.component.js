12323eeac9c8fa93061b2731608d1023d2e2b174
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-12-14 12:00:08 +0700

    .

--- src/components/Circle/Circle.component.js
+++ src/components/Circle/Circle.component.js
@@ -5,7 +5,7 @@ import {Icon} from 'react-fa';
 
 class Circle extends Component {
   render () {
-    const {iconName, colorStyle} = this.props;
+    const {iconName, imageSource, colorStyle} = this.props;
     return (
       <div className={`icon ${colorStyle ? 'icon__' + colorStyle : 'icon__gray'}`}>
         <Icon name={iconName} />
