6b366abcec974cf58c8973e6f095ef646687491a
Benjapol Suksakulchai <assassin@Benjapols-iMac.local>
2017-12-14 11:57:44 +0700

    Commit before Do ejct

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,4 +1,4 @@
-import './Header.style.css';
+import headerStyles from './Header.style.css';
 import React, {Component} from 'react';
 import Circle from '../Circle/Circle.component';
 import {Icon} from 'react-fa';
@@ -6,15 +6,15 @@ import Search from '../Search/Search.component';
 class Header extends Component {
   render () {
     return (
-      <div className="app-header shadow white-box" >
-        <div className="left-content"> 
-          <div className="before-left"> <Circle color="twitter"><Icon name='twitter' /></Circle></div>
+      <div className={headerStyles.appHeader} >
+        <div className={headerStyles.leftContent}> 
+          <div className={headerStyles.beforeLeft}> <Circle color="twitter"><Icon name='twitter' /></Circle></div>
           <Circle color="" margin="margin-l"><Icon name='bell-o' /></Circle>
           <Circle color="" margin="margin-l"><Icon name='envelope-o' /></Circle>
           <Circle color="" margin="margin-l"><Icon name='hashtag' /></Circle>
         </div>
-        <div className="logo"> <Icon name='twitter' size='2x'  /></div>
-        <div className="right-content">
+        <div className={headerStyles.logo}> <Icon name='twitter' size='2x'  /></div>
+        <div className={headerStyles.rightContent}>
           <Search/>
           <Circle margin="margin-rl"><img src="http://icons.iconarchive.com/icons/designbolts/despicable-me-2/32/Minion-Amazed-icon.png"/></Circle>
         </div>
