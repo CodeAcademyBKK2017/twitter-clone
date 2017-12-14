14f1ab4039e4463407d1c90cf644e02ded679d4a
chaowalit <dekchaowalit_123@hotmail.com>
2017-12-14 14:57:58 +0700

    update icon

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,7 +1,7 @@
 import headerStyle from './Header.css';
 import circleStyle from '../Circle/Circle.css';
 import React, {Component} from 'react';
-import {Icon} from 'react-fa';
+import Icon from 'react-ionicons';
 import Circle from '../Circle/Circle.component';
 import classNames from 'classnames';
 
@@ -13,34 +13,34 @@ class Header extends Component {
         <div className={headerStyle.left_head}>
           <div className={headerStyle.head_first_icon}>
             <Circle circle={classNames(circleStyle.circle, circleStyle.background_sky)}>
-              <Icon name="twitter" />
+              <Icon icon="logo-twitter" fontSize="30px" color="white"/>
             </Circle>
           </div>
           <div className={headerStyle.head_second_icon}>
             <Circle circle={circleStyle.circle}>
-              <Icon name="bell-o" />
+              <Icon icon="ios-notifications-outline" fontSize="30px"/>
             </Circle>
             <Circle circle={circleStyle.circle}>
-              <Icon name="envelope-o" />
+              <Icon icon="ios-mail-outline" fontSize="30px"/>
             </Circle>
             <Circle circle={circleStyle.circle}>
-              <Icon name="hashtag" />
+              <Icon icon="ios-grid-outline" fontSize="30px"/>
             </Circle>
           </div>
         </div>
         <div className={headerStyle.center_head}>
           <Circle circle="">
-            <Icon name="twitter" size="2x"/>
+            <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
           </Circle>
         </div>
         <div className={headerStyle.right_head}>
           <div className={headerStyle.right_head_text}>
             <input type="text"/>
-            <Icon name="search"/>
+            <Icon icon="ios-search-outline"/>
           </div>
           <div className={headerStyle.right_head_icon}>
             <Circle circle={circleStyle.circle}>
-              <Icon name="user-o" />
+              <Icon icon="logo-github" fontSize="30px"/>
             </Circle>
           </div>
         </div>
