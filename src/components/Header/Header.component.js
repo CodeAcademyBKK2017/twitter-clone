28ce505d4d366d1f9ec2e0e46df3cae413b8f174
Benjapol Suksakulchai <assassin@Benjapols-iMac.local>
2017-12-14 14:37:05 +0700

    Done with the ionicon logo

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,19 +1,21 @@
 import headerStyles from './Header.style.css';
 import React, {Component} from 'react';
 import Circle from '../Circle/Circle.component';
-import {Icon} from 'react-fa';
+import Icon from 'react-ionicons';
 import Search from '../Search/Search.component';
 class Header extends Component {
   render () {
     return (
       <div className={headerStyles.appHeader} >
         <div className={headerStyles.leftContent}> 
-          <div className={headerStyles.beforeLeft}> <Circle type="twitter"><Icon name='twitter' /></Circle></div>
-          <Circle type="normalL"><Icon name='bell-o' /></Circle>
-          <Circle type="normalL"><Icon name='envelope-o' /></Circle>
-          <Circle type="normalL"><Icon name='hashtag' /></Circle>
+          <div className={headerStyles.beforeLeft}> 
+            <Circle type="twitter"><Icon icon='logo-twitter' fonSize="35px" color="white" /></Circle>
+          </div>
+          <Circle type="normalL"><Icon icon='ios-notifications-outline' /></Circle>
+          <Circle type="normalL"><Icon icon='ios-mail-outline' /></Circle>
+          <Circle type="normalL"><Icon icon='ios-pricetag' /></Circle>
         </div>
-        <div className={headerStyles.logo}> <Icon name='twitter' size='2x'  /></div>
+        <div className={headerStyles.logo}> <Icon icon='logo-twitter' fonSize='35px' color="#00aced"  /></div>
         <div className={headerStyles.rightContent}>
           <Search/>
           <Circle type="normalRL"><img src="http://icons.iconarchive.com/icons/designbolts/despicable-me-2/32/Minion-Amazed-icon.png"/></Circle>
--- src/components/Search/Search.component.js
+++ src/components/Search/Search.component.js
@@ -1,13 +1,13 @@
 import searchStyle from './Search.style.css';
 import React, {Component} from 'react';
-import {Icon} from 'react-fa';
+import Icon from 'react-ionicons';
 class Search extends Component {
   render () {
     return (
       <div className={searchStyle.search} >
         <input className={searchStyle.input} />
         <div className={searchStyle.searchIcon}>
-          <Icon name='search' />
+          <Icon icon='ios-search' />
         </div>
       </div>
     );
