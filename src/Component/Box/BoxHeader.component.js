a445708ff152b6c98cc633602e56ded74b5c7be5
nubrabra <nubrabra5@gmail.com>
2017-12-15 10:33:43 +0700

    15122017V1

--- src/Component/Box/BoxHeader.component.js
+++ src/Component/Box/BoxHeader.component.js
@@ -6,10 +6,22 @@ class BoxHeader extends Component {
   render () {
     return (
       <div className={classNames(inputStyles.BoxTitle, inputStyles.Shadow)}>
-        <div />
-        <div />
-        <div />
-        <div />
+        <div className={inputStyles.boxHeaderContent}>
+          <span className={inputStyles.textHeaderTitle}>{'TWEETS'}</span>
+          <span className={inputStyles.textHeaderNumber}>{'200'}</span>
+        </div>
+        <div className={inputStyles.boxHeaderContent}>
+          <span className={inputStyles.textHeaderTitle}>{'PHOTOS/VIDEOS'}</span>
+          <span className={inputStyles.textHeaderNumber}>{'200'}</span>
+        </div>
+        <div className={inputStyles.boxHeaderContent}>
+          <span className={inputStyles.textHeaderTitle}>{'FOLLOWINGS'}</span>
+          <span className={inputStyles.textHeaderNumber}>{'200'}</span>
+        </div>
+        <div className={inputStyles.boxHeaderContent}>
+          <span className={inputStyles.textHeaderTitle}>{'FOLLOWERS'}</span>
+          <span className={inputStyles.textHeaderNumber}>{'1M'}</span>
+        </div>
       </div>
     );
   }
