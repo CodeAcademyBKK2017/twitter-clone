73c9220c76eb1207cef4f733420f48f641184814
Benjapol Suksakulchai <assassin@Benjapols-iMac.local>
2017-12-15 11:47:55 +0700

    Update mobile support layout

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -6,19 +6,29 @@ import Search from '../Search/Search.component';
 class Header extends Component {
   render () {
     return (
-      <div className={headerStyles.appHeader} >
-        <div className={headerStyles.leftContent}> 
-          <div className={headerStyles.beforeLeft}> 
-            <Circle type="twitter"><Icon icon='logo-twitter' fonSize="35px" color="white" /></Circle>
+      <div>
+        <div className={headerStyles.appHeader} >
+          <div className={headerStyles.leftContent}> 
+            <div className={headerStyles.beforeLeft}> 
+              <Circle type="twitter"><Icon icon='logo-twitter' fonSize="35px" color="white" /></Circle>
+            </div>
+            <Circle type="normalL"><Icon icon='ios-notifications-outline' /></Circle>
+            <Circle type="normalL"><Icon icon='ios-mail-outline' /></Circle>
+            <Circle type="normalL"><Icon icon='ios-grid-outline' /></Circle>
           </div>
-          <Circle type="normalL"><Icon icon='ios-notifications-outline' /></Circle>
-          <Circle type="normalL"><Icon icon='ios-mail-outline' /></Circle>
-          <Circle type="normalL"><Icon icon='ios-grid-outline' /></Circle>
+          <div className={headerStyles.logo}> <Icon icon='logo-twitter' fonSize='35px' color="#00aced"  /></div>
+          <div className={headerStyles.rightContent}>
+            <Search/>
+            <Circle type="normalRL"><img src="http://icons.iconarchive.com/icons/designbolts/despicable-me-2/32/Minion-Amazed-icon.png"/></Circle>
+          </div>
         </div>
-        <div className={headerStyles.logo}> <Icon icon='logo-twitter' fonSize='35px' color="#00aced"  /></div>
-        <div className={headerStyles.rightContent}>
-          <Search/>
-          <Circle type="normalRL"><img src="http://icons.iconarchive.com/icons/designbolts/despicable-me-2/32/Minion-Amazed-icon.png"/></Circle>
+        <div className={headerStyles.appHeaderMobile} >
+          <div className={headerStyles.sidemenu} >
+            <Icon icon='ios-menu' fonSize="50px" color="#00aced" />
+          </div>
+          <div className={headerStyles.logo}>
+            <Icon icon='logo-twitter' fonSize="50px" color="#00aced" />
+          </div>
         </div>
       </div>
     );
