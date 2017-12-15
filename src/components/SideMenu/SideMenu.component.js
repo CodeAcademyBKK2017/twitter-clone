017e54b58c5a98d333c55ea1c1f0a8fbbe51d68f
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-12-15 16:17:52 +0700

    use classNames with dynamic key in object

--- src/components/SideMenu/SideMenu.component.js
+++ src/components/SideMenu/SideMenu.component.js
@@ -2,18 +2,18 @@ import SideMenuStyle from './SideMenu.style.css';
 import React, {Component} from 'react';
 import SearchBar from '../SearchBar/SearchBar.component';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 class SideMenu extends Component {
 	render () {
 		const {isHidden} = this.props;
-
 		return (
-			<div className={`${SideMenuStyle.container} ${isHidden ? SideMenuStyle.hidden : ''}`}>
+			<div className={classNames(SideMenuStyle.container, {[SideMenuStyle.hidden]: isHidden})}>
 				<div className={SideMenuStyle.searchBarContainer}>
 					<SearchBar isInSideMenu={true}/>
 				</div>
 				
-				<div className={SideMenuStyle.menuContainer} />
+				<div className={SideMenuStyle.menuContainer}/>
 
 				<div className={SideMenuStyle.creditContainer}>
 					<b>Made by: Chong</b>
