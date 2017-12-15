4fbc007c3b2e6de8653408446cb9ccb922131d38
chaowalit <dekchaowalit_123@hotmail.com>
2017-12-15 16:23:09 +0700

    update sidebar menu animation

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -7,74 +7,82 @@ import classNames from 'classnames';
 
 
 class Header extends Component {
-  render () {
-    return (
-      <div>
-        <div className={classNames(headerStyle.header, headerStyle.box_shadow, headerStyle.background_white)}>
-          <div className={headerStyle.left_head}>
-            <div className={headerStyle.head_first_icon}>
-              <Circle circle={classNames(circleStyle.circle, circleStyle.background_sky)}>
-                <Icon icon="logo-twitter" fontSize="30px" color="white"/>
-              </Circle>
-            </div>
-            <div className={headerStyle.head_second_icon}>
-              <div>
-                <Circle circle={circleStyle.circle}>
-                  <Icon icon="ios-notifications-outline" fontSize="30px"/>
+    state = {
+      showVisible: false
+    }
+    menuclickHandler = () => {
+      this.setState({showVisible: !this.state.showVisible});
+    }
+
+    render () {
+      return (
+        <div>
+          <div className={classNames(headerStyle.header, headerStyle.box_shadow, headerStyle.background_white)}>
+            <div className={headerStyle.left_head}>
+              <div className={headerStyle.head_first_icon}>
+                <Circle circle={classNames(circleStyle.circle, circleStyle.background_sky)}>
+                  <Icon icon="logo-twitter" fontSize="30px" color="white"/>
                 </Circle>
               </div>
-              <Circle circle={circleStyle.circle}>
-                <Icon icon="ios-mail-outline" fontSize="30px"/>
-              </Circle>
-              <Circle circle={circleStyle.circle}>
-                <Icon icon="ios-grid-outline" fontSize="30px"/>
+              <div className={headerStyle.head_second_icon}>
+                <div>
+                  <Circle circle={circleStyle.circle}>
+                    <Icon icon="ios-notifications-outline" fontSize="30px"/>
+                  </Circle>
+                </div>
+                <Circle circle={circleStyle.circle}>
+                  <Icon icon="ios-mail-outline" fontSize="30px"/>
+                </Circle>
+                <Circle circle={circleStyle.circle}>
+                  <Icon icon="ios-grid-outline" fontSize="30px"/>
+                </Circle>
+              </div>
+            </div>
+            <div className={headerStyle.center_head}>
+              <Circle circle="">
+                <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
               </Circle>
             </div>
+            <div className={headerStyle.right_head}>
+              <div className={headerStyle.right_head_text}>
+                <input type="text"/>
+                <Icon icon="ios-search-outline"/>
+              </div>
+              <div className={headerStyle.right_head_icon}>
+                <Circle circle={circleStyle.circle}>
+                  <Icon icon="logo-github" fontSize="30px"/>
+                </Circle>
+              </div>
+            </div>
           </div>
-          <div className={headerStyle.center_head}>
-            <Circle circle="">
-              <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
-            </Circle>
+
+          <div className={classNames(headerStyle.header_mobile, headerStyle.box_shadow, headerStyle.background_white)}>
+            <div className={headerStyle.left_head} onClick={this.menuclickHandler}>
+              <Circle circle={classNames(circleStyle.circle)}>
+                <Icon icon="ios-menu" fontSize="30px"/>
+              </Circle>
+            </div>
+            <div className={headerStyle.center_head}>
+              <Circle circle="">
+                <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
+              </Circle>
+            </div>
+            <div className={headerStyle.right_head} />
           </div>
-          <div className={headerStyle.right_head}>
-            <div className={headerStyle.right_head_text}>
+        
+          <div className={classNames(headerStyle.menuBar, {[headerStyle.activeMenu]: this.state.showVisible})}>
+            <div className={classNames(headerStyle.right_head_text, headerStyle.searchTextBox)}>
               <input type="text"/>
               <Icon icon="ios-search-outline"/>
             </div>
-            <div className={headerStyle.right_head_icon}>
-              <Circle circle={circleStyle.circle}>
-                <Icon icon="logo-github" fontSize="30px"/>
-              </Circle>
+
+            <div className={classNames(headerStyle.bottomText)}>
+              Chaowalit Kongkham
             </div>
           </div>
         </div>
-
-        <div className={classNames(headerStyle.header_mobile, headerStyle.box_shadow, headerStyle.background_white)}>
-          <div className={headerStyle.left_head}>
-            <Circle circle={classNames(circleStyle.circle)}>
-              <Icon icon="ios-menu" fontSize="30px"/>
-            </Circle>
-          </div>
-          <div className={headerStyle.center_head}>
-            <Circle circle="">
-              <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
-            </Circle>
-          </div>
-          <div className={headerStyle.right_head} />
-        </div>
-        <div className={classNames(headerStyle.menuBar)}>
-          <div className={classNames(headerStyle.right_head_text, headerStyle.searchTextBox)}>
-            <input type="text"/>
-            <Icon icon="ios-search-outline"/>
-          </div>
-
-          <div className={classNames(headerStyle.bottomText)}>
-              Chaowalit Kongkham
-          </div>
-        </div>
-      </div>
-    );
-  }
+      );
+    }
 }
 
 export default Header;
