68b42e3ffacceb26c59da16322c212e5cdb0e0b2
nanny <nanny@nannys-iMac.local>
2017-12-15 16:11:05 +0700

    web

--- src/Component/Header/Header.component.js
+++ src/Component/Header/Header.component.js
@@ -7,19 +7,28 @@ import Icon from 'react-ionicons';
 import className from 'classname';
 
 class Header extends Component {
+  state = {
+    menuVisibility: false
+  }
+  toggleMenu =() => {
+    this.setState({menuVisibility: !this.state.menuVisibility});
+  }
   render () {
     return (
       <div className={className(inputStyles.header, inputStyles.shadow)}>
-        <div className={className(inputStyles.sidemenu)}>
-          <div className={inputStyles.borderStyle}>
-            <input className={inputStyles.inputStyle} placeholder="Search"/>
-            <Icon className={inputStyles.colorStyle} icon='ios-search'/>
-          </div>
-          <div className={inputStyles.name}>Nanny</div>
-        </div>
-        <div className={className(inputStyles.show)}>
-          <Icon icon="ios-menu"/>
-        </div>
+        { 
+          this.state.menuVisibility ?
+            <div className={className(inputStyles.sidemenu)}>
+              <div className={inputStyles.borderStyle}>
+                <input className={inputStyles.inputStyle} placeholder="Search"/>
+                <Icon className={inputStyles.colorStyle} icon='ios-search'/>
+              </div>
+              <div className={inputStyles.name}>Nanny</div>
+            </div> : null
+        }
+        
+        <div className={className(inputStyles.show)} onClick={this.toggleMenu}>
+          <Icon icon="ios-menu"/></div> 
         <div className={className(inputStyles.flex4, inputStyles.contentLeft)}>
         
           <CircleBG iconName={'logo-twitter'} color='white'/>
