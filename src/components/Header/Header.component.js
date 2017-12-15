7a392e9f78f4aec44ee42a1f642715024359aaae
chaowalit <dekchaowalit_123@hotmail.com>
2017-12-15 14:08:12 +0700

    update css responsive

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -9,42 +9,58 @@ import classNames from 'classnames';
 class Header extends Component {
   render () {
     return (
-      <div className={classNames(headerStyle.header, headerStyle.box_shadow, headerStyle.background_white)}>
-        <div className={headerStyle.left_head}>
-          <div className={headerStyle.head_first_icon}>
-            <Circle circle={classNames(circleStyle.circle, circleStyle.background_sky)}>
-              <Icon icon="logo-twitter" fontSize="30px" color="white"/>
-            </Circle>
-          </div>
-          <div className={headerStyle.head_second_icon}>
-            <div>
-              <Circle circle={circleStyle.circle}>
-                <Icon icon="ios-notifications-outline" fontSize="30px"/>
+      <div>
+        <div className={classNames(headerStyle.header, headerStyle.box_shadow, headerStyle.background_white)}>
+          <div className={headerStyle.left_head}>
+            <div className={headerStyle.head_first_icon}>
+              <Circle circle={classNames(circleStyle.circle, circleStyle.background_sky)}>
+                <Icon icon="logo-twitter" fontSize="30px" color="white"/>
               </Circle>
             </div>
-            <Circle circle={circleStyle.circle}>
-              <Icon icon="ios-mail-outline" fontSize="30px"/>
+            <div className={headerStyle.head_second_icon}>
+              <div>
+                <Circle circle={circleStyle.circle}>
+                  <Icon icon="ios-notifications-outline" fontSize="30px"/>
+                </Circle>
+              </div>
+              <Circle circle={circleStyle.circle}>
+                <Icon icon="ios-mail-outline" fontSize="30px"/>
+              </Circle>
+              <Circle circle={circleStyle.circle}>
+                <Icon icon="ios-grid-outline" fontSize="30px"/>
+              </Circle>
+            </div>
+          </div>
+          <div className={headerStyle.center_head}>
+            <Circle circle="">
+              <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
             </Circle>
-            <Circle circle={circleStyle.circle}>
-              <Icon icon="ios-grid-outline" fontSize="30px"/>
-            </Circle>
+          </div>
+          <div className={headerStyle.right_head}>
+            <div className={headerStyle.right_head_text}>
+              <input type="text"/>
+              <Icon icon="ios-search-outline"/>
+            </div>
+            <div className={headerStyle.right_head_icon}>
+              <Circle circle={circleStyle.circle}>
+                <Icon icon="logo-github" fontSize="30px"/>
+              </Circle>
+            </div>
           </div>
         </div>
-        <div className={headerStyle.center_head}>
-          <Circle circle="">
-            <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
-          </Circle>
-        </div>
-        <div className={headerStyle.right_head}>
-          <div className={headerStyle.right_head_text}>
-            <input type="text"/>
-            <Icon icon="ios-search-outline"/>
-          </div>
-          <div className={headerStyle.right_head_icon}>
-            <Circle circle={circleStyle.circle}>
-              <Icon icon="logo-github" fontSize="30px"/>
+
+        <div className={classNames(headerStyle.header_mobile, headerStyle.box_shadow, headerStyle.background_white)}>
+          <div className={headerStyle.left_head}>
+            <Circle circle={classNames(circleStyle.circle)}>
+              <Icon icon="ios-menu" fontSize="30px"/>
             </Circle>
           </div>
+          <div className={headerStyle.center_head}>
+            <Circle circle="">
+              <Icon icon="logo-twitter" fontSize="30px" color="#80A8CC"/>
+            </Circle>
+          </div>
+          <div className={headerStyle.right_head} />
         </div>
       </div>
     );
--- src/components/TapBar/TapBar.component.js
+++ src/components/TapBar/TapBar.component.js
@@ -7,21 +7,21 @@ class Tapbar extends Component {
   render () {
     return (
       <div className={classNames(TapbarStyle.second_container, TapbarStyle.box_shadow, TapbarStyle.background_white)}>
-        <div className={TapbarStyle.box_tweets}>
+        <div className={classNames(TapbarStyle.box_tweets, TapbarStyle.box_tweets_first)}>
           <div className={TapbarStyle.box_tweets_item1}>TWEETS</div>
           <div className={TapbarStyle.box_tweets_item2}>200</div>
         </div>
-        <div className={TapbarStyle.box_photos}>
-          <div className={TapbarStyle.box_photos_item1}>PHOTOS/VIDEOS</div>
-          <div className={TapbarStyle.box_photos_item2}>200</div>
+        <div className={TapbarStyle.box_tweets}>
+          <div className={TapbarStyle.box_tweets_item1}>PHOTOS/VIDEOS</div>
+          <div className={TapbarStyle.box_tweets_item2}>200</div>
         </div>
-        <div className={TapbarStyle.box_following}>
-          <div className={TapbarStyle.box_following_item1}>FOLLOWING</div>
-          <div className={TapbarStyle.box_following_item2}>200</div>
+        <div className={TapbarStyle.box_tweets}>
+          <div className={TapbarStyle.box_tweets_item1}>FOLLOWING</div>
+          <div className={TapbarStyle.box_tweets_item2}>200</div>
         </div>
-        <div className={TapbarStyle.box_followers}>
-          <div className={TapbarStyle.box_followers_item1}>FOLLOWERS</div>
-          <div className={TapbarStyle.box_followers_item2}>1M</div>
+        <div className={TapbarStyle.box_tweets}>
+          <div className={TapbarStyle.box_tweets_item1}>FOLLOWERS</div>
+          <div className={TapbarStyle.box_tweets_item2}>1M</div>
         </div>
       </div>
     );
