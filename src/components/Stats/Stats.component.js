ae488d29a85acf1aa0dc5f5c0052e65ed5117a55
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-12-15 12:35:51 +0700

    2 row 2 column

--- src/components/Stats/Stats.component.js
+++ src/components/Stats/Stats.component.js
@@ -5,22 +5,22 @@ import statsStyle from './Stats.style.css';
 class Stats extends Component {
   render () {
     return (
-      <div className={classNames(statsStyle.navContainer)}>
-        <div className={classNames(statsStyle.nav, statsStyle.active)}>
-          <div className={classNames(statsStyle.nav__name)}>tweets</div>
-          <div className={classNames(statsStyle.nav__value)}>200</div>
+      <div className={classNames(statsStyle.itemContainer)}>
+        <div className={classNames(statsStyle.item, statsStyle.active)}>
+          <div className={classNames(statsStyle.item__name)}>tweets</div>
+          <div className={classNames(statsStyle.item__value)}>200</div>
         </div>
-        <div className={classNames(statsStyle.nav)}>
-          <div className={classNames(statsStyle.nav__name)}>photos/videos</div>
-          <div className={classNames(statsStyle.nav__value)}>200</div>
+        <div className={classNames(statsStyle.item)}>
+          <div className={classNames(statsStyle.item__name)}>photos/videos</div>
+          <div className={classNames(statsStyle.item__value)}>200</div>
         </div>
-        <div className={classNames(statsStyle.nav)}>
-          <div className={classNames(statsStyle.nav__name)}>following</div>
-          <div className={classNames(statsStyle.nav__value)}>200</div>
+        <div className={classNames(statsStyle.item)}>
+          <div className={classNames(statsStyle.item__name)}>following</div>
+          <div className={classNames(statsStyle.item__value)}>200</div>
         </div>
-        <div className={classNames(statsStyle.nav)}>
-          <div className={classNames(statsStyle.nav__name)}>followers</div>
-          <div className={classNames(statsStyle.nav__value)}>1m</div>
+        <div className={classNames(statsStyle.item)}>
+          <div className={classNames(statsStyle.item__name)}>followers</div>
+          <div className={classNames(statsStyle.item__value)}>1m</div>
         </div>
       </div>
     );
